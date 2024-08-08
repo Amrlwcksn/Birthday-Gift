12ba9ae88add9729afeef6d5a4a2f67613857b44
AmirulWicaksono <156315329+Amrlwcksn@users.noreply.github.com>
2024-08-08 19:41:02 +0700

    Menambahkan fitur buzzer

--- Sketch 1/Sketchinteraktif.ino
+++ Sketch 1/Sketchinteraktif.ino
@@ -16,6 +16,121 @@ boolean OK   = false;
 int halaman  = 1;
 int menuItem = 1;
 
+//note buzzer
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+int tempo = 145;
+int buzzer = 26;
+int melody[] = {
+
+  NOTE_C4,4, NOTE_C4,8, 
+  NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
+  NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8, 
+  NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
+  NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
+
+  NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4, 
+  NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
+  NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
+  NOTE_F4,-2,
+ 
+};
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
+
+
 //date
 #define FRAME_DELAY (10)
 #define FRAME_WIDTH (32)
@@ -141,7 +256,7 @@ void loop() {
 
   if (OK) {
 OK = false;
-    if (halaman == 1 && menuItem == 1) {
+    if (halaman == 1 && halaman == 1) {
       halaman = 2;
     } else if (halaman == 2 && halaman == 2) {
       halaman = 3;
@@ -156,6 +271,8 @@ OK = false;
 
   Serial.println(menuItem);
   delay(100);
+
+   
 
 }
 
@@ -178,7 +295,7 @@ void tampil() {
     lcd.setCursor(40, 5);
     lcd.print("Haii Fatma");
 
-    if (menuItem == 1) {   //Jam
+    if (halaman == 1) {   //Jam
       lcd.setTextSize(1);
       lcd.setCursor(46, 20);
       lcd.setTextColor(WHITE);
@@ -199,7 +316,7 @@ void tampil() {
     lcd.setCursor(35,15);
     lcd.print("berapa yaa");
     lcd.setCursor(35,25);
-    lcd.print("<(Tekan)>");
+    lcd.print("<(Next)>");
    
 
     lcd.drawBitmap(0, 0, date1[frame], FRAME_WIDTH, FRAME_HEIGHT, 1);
@@ -216,7 +333,7 @@ void tampil() {
     lcd.setCursor(35, 5);
     lcd.print("29 Sept 2024");
     lcd.setCursor(35,15);
-    lcd.print("<(Tekan)>");
+    lcd.print("<(Next)>");
 
     lcd.drawBitmap(0, 0, date2[frame], FRAME_WIDTH, FRAME_HEIGHT, 1);
     lcd.display();
@@ -234,7 +351,7 @@ void tampil() {
     lcd.setCursor(0,10);
     lcd.print("Fatma...");
     lcd.setCursor(0,20);
-    lcd.print("<(Tekan)>");
+    lcd.print("<(Next)>");
     
   }
   else if (halaman == 5) {
@@ -247,14 +364,27 @@ void tampil() {
     lcd.print("Potong kuenya");
     lcd.setTextSize(1);
     lcd.setCursor(35,15);
-    lcd.print("<(Tekan)>");
-   
+    lcd.print("<(Potong)>");
+
 
     lcd.drawBitmap(0, 0, cake[frame], FRAME_WIDTH, FRAME_HEIGHT, 1);
     lcd.display();
     frame = (frame + 1) % FRAME_COUNT;
     delay(FRAME_DELAY);
-    
+
+     for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+      divider = melody[thisNote + 1];
+     if (divider > 0) {
+       noteDuration = (wholenote) / divider;
+      } else if (divider < 0) {
+       noteDuration = (wholenote) / abs(divider);
+        noteDuration *= 1.5;
+      }
+      tone(buzzer, melody[thisNote], noteDuration * 0.9);
+      delay(noteDuration);
+      noTone(buzzer);
+    }
+
   }
    else if (halaman == 6) {
     lcd.clearDisplay();
