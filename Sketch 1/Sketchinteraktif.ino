55409981f35bf51f55eee055b1deff6d39e3ebe0
AmirulWicaksono <156315329+Amrlwcksn@users.noreply.github.com>
2024-09-12 22:16:49 +0700

    menambahkan intro hbd
    memindahkan note hbd ke void Setup

--- Sketch 1/Sketchinteraktif.ino
+++ Sketch 1/Sketchinteraktif.ino
@@ -16,121 +16,6 @@ boolean OK   = false;
 int halaman  = 1;
 int menuItem = 1;
 
-//note buzzer
-
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-#define REST      0
-
-
-int tempo = 145;
-int buzzer = 26;
-int melody[] = {
-
-  NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
-  NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
-  NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
-
-  NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4, 
-  NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
-  NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
-  NOTE_F4,-2,
- 
-};
-int notes = sizeof(melody) / sizeof(melody[0]) / 2;
-int wholenote = (60000 * 4) / tempo;
-int divider = 0, noteDuration = 0;
-
-
 //date
 #define FRAME_DELAY (10)
 #define FRAME_WIDTH (32)
@@ -238,11 +123,150 @@ const byte PROGMEM cake[][128] = {
   {0,0,0,0,0,1,0,0,0,1,128,0,0,3,192,0,0,6,96,0,0,4,32,0,0,4,32,0,0,5,160,0,0,3,192,0,0,1,128,0,0,3,192,0,0,7,224,0,0,4,32,0,0,4,32,0,0,4,32,0,0,4,32,0,0,4,32,0,0,4,32,0,7,255,255,224,12,0,0,48,24,0,0,24,24,0,0,24,24,48,12,24,28,78,51,56,31,7,193,216,16,0,0,8,24,0,0,24,127,255,255,254,96,0,0,6,63,255,255,252,31,255,255,248,0,0,0,0}
 };
 
+//buzzer happy birthday
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+int tempo = 140;
+int buzzer = 26;
+int melody[] = {
+
+  NOTE_C4,4, NOTE_C4,8, 
+  NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
+  NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8, 
+  NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
+  NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
+
+  NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4, 
+  NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
+  NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
+  NOTE_F4,-2,
+ 
+};
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
 
 void setup() {
   Serial.begin(115200);
+
+  
+
   lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   lcd.clearDisplay();
+
+    lcd.setTextSize(1);
+    lcd.setTextColor(WHITE);
+    lcd.setCursor(17, 5);
+    lcd.print("Happy Birthday...");
+    lcd.display();
+    delay(1000);
+    lcd.clearDisplay();
+
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(buzzer);
+  }
+
+    
+  
 
   pinMode(btnOk,   INPUT_PULLUP);
 }
@@ -266,6 +290,8 @@ OK = false;
       halaman = 5;
     } else if (halaman == 5 && halaman == 5) {
       halaman = 6;
+    }else if (halaman == 6 && halaman == 6) {
+      halaman = 7;
     }
   }
 
@@ -371,38 +397,34 @@ void tampil() {
     lcd.display();
     frame = (frame + 1) % FRAME_COUNT;
     delay(FRAME_DELAY);
-
-     for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-      divider = melody[thisNote + 1];
-     if (divider > 0) {
-       noteDuration = (wholenote) / divider;
-      } else if (divider < 0) {
-       noteDuration = (wholenote) / abs(divider);
-        noteDuration *= 1.5;
-      }
-      tone(buzzer, melody[thisNote], noteDuration * 0.9);
-      delay(noteDuration);
-      noTone(buzzer);
-    }
-
   }
    else if (halaman == 6) {
     lcd.clearDisplay();
     lcd.setTextSize(1);
     lcd.setTextColor(WHITE);
     lcd.setCursor(0, 0);
-    lcd.print("naa sekarang kuenya");
+    lcd.print("yeyyy, udah di potong");
     lcd.setTextSize(1);
     lcd.setCursor(0,10);
-    lcd.print("di bagi dong, ms icak");
-    lcd.setCursor(0,20);
-    lcd.print("minta disuapin tuh wk");
+    lcd.print("<(Next)>");
   }
+  else if (halaman == 7) {
+    lcd.clearDisplay();
+    lcd.setTextSize(1);
+    lcd.setTextColor(WHITE);
+    lcd.setCursor(0, 0);
+    lcd.print("Semoga panjang umur,");
+    lcd.setTextSize(1);
+    lcd.setCursor(0,10);
+    lcd.print("nambah imut,nambah");
+    lcd.setCursor(0,20);
+    lcd.print("lucu-  <(Next)>  ");
+  }
+
 
 
  
 
   lcd.display();
 }
-
 
